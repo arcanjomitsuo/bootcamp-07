72c7826eb3d6047c2a16c6152333a87bdf03103a
arcanjomitsuo <arcanjomitsuo@gmail.com>
2020-01-29 22:04:04 -0300

    Cadastro de usuários

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/Users';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Rodrigo',
-    email: 'arcanjomitsuo@gmail.com',
-    password_hash: '123456',
-  });
-
-  return res.json({ message: 'Hello' });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
