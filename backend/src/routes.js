d558170eb6f7d8117a1eae4dd57eafad024714da
arcanjomitsuo <arcanjomitsuo@gmail.com>
2020-01-29 21:50:12 -0300

    Criando loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/Users';
 
 const routes = new Router();
+
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Rodrigo',
+    email: 'arcanjomitsuo@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json({ message: 'Hello' });
+});
 
 export default routes;
