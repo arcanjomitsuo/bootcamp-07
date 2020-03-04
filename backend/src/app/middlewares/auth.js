853cd7c1c42e95e02537f1be71dd5de7dc847951
arcanjomitsuo <arcanjomitsuo@gmail.com>
2020-03-03 22:52:42 -0300

    Configurando Multer

--- backend/src/app/middlewares/auth.js
+++ backend/src/app/middlewares/auth.js
@@ -16,7 +16,7 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     req.userId = decoded.id;
   } catch (err) {
-    return res.status(4010).json({ error: 'Token invalid' });
+    return res.status(401).json({ error: 'Token invalid' });
   }
 
   return next();
