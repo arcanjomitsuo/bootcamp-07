574fb17cdd7705fa62487666a0ca2cb8ad90aa5c
arcanjomitsuo <arcanjomitsuo@gmail.com>
2020-03-05 23:30:48 -0300

    Configurando Nodemailer

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4eb0d71bd9d0c4',
+    pass: 'a7d5672519d447',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
